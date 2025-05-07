8fef852ff9f0a944e6dbdfe181aa12fb44adf2e0
Baqbergen99 <bahabai35015@gmail.com>
2025-05-07 22:43:25 +0400

    Interface

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,7 @@ class Task(models.Model):
    title = models.CharField(max_length=255) # Название задачи
    description = models.TextField(blank=True, null=True) # Описание
    created_at = models.DateTimeField(auto_now_add=True) # Дата создания
+   image_url = models.URLField(blank=True, null=True) # Поле для ссылки на изображение
  
    def __str__(self):
      return self.title
